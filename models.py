e0dd2bf1271792ca8c860ccb54079289e85b0cb7
smithhx <christian.smith.stanley@gmail.com>
2020-10-04 15:16:40 -0400

    import  os

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
-
+import os
 
 # database_path = 'postgres://smith@localhost:5432/capstone'
 database_path = os.environ['DATABASE_URL']
