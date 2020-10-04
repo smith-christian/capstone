14eb62fdfe11984cc90fcc8a6aa0a91c375161d5
smithhx <christian.smith.stanley@gmail.com>
2020-10-04 14:31:04 -0400

    modified readme and setup datbase_url

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ AUTH0_DOMAIN = 'sc-fsnd.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'capstone'
 #DATABASE_URL = 'postgres://smith@localhost:5432/capstone'
-DATABASE_URL = 'postgres://kvxnpwonsoppio:0237482f950f4c504f8c36bb0c0aa05afc841f884fc9b1d2810e36ead36df8ca@ec2-3-224-97-209.compute-1.amazonaws.com:5432/d9ln84e80n14k2'
+DATABASE_URL = 'postgres://sbuwbkmqasuuyj:fae14526bbf19af459ebbfe4a6be3a3e9623de8edd48d733b61e5c71cc0de87e@ec2-54-156-53-71.compute-1.amazonaws.com:5432/d3487jgh6c19qg'
