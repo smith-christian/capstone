059f36b8d9c29856ad39beae028408e34cf578a4
smithhx <christian.smith.stanley@gmail.com>
2020-10-04 12:45:05 -0400

    make it better

---
+++ setup.sh
@@ -0,0 +1,5 @@
+AUTH0_DOMAIN = 'sc-fsnd.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'capstone'
+#DATABASE_URL = 'postgres://smith@localhost:5432/capstone'
+DATABASE_URL = 'postgres://kvxnpwonsoppio:0237482f950f4c504f8c36bb0c0aa05afc841f884fc9b1d2810e36ead36df8ca@ec2-3-224-97-209.compute-1.amazonaws.com:5432/d9ln84e80n14k2'
