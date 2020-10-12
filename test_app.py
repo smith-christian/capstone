cd60a43fd9df94f27581ceec45d8131f3cf415f4
smithhx <christian.smith.stanley@gmail.com>
2020-10-12 10:59:01 -0400

    commit test_ap.py

--- test_app.py
+++ test_app.py
@@ -7,6 +7,8 @@ from app import create_app
 from models import setup_db, Movie, Actor
 
 ASSISTANT_TOKEN = os.environ.get('ASSISTANT_TOKEN') 
+DIRECTOR_TOKEN = os.environ.get('DIRECTOR_TOKEN')
+PRODUCER_TOKEN = os.environ.get('PRODUCER_TOKEN')
 
 class CapstoneTestCase(unittest.TestCase):
     """This class represents the capstone test case"""
@@ -22,25 +24,59 @@ class CapstoneTestCase(unittest.TestCase):
 
         self.assistant_header = {
             "Authorization": "Bearer {}".format(ASSISTANT_TOKEN)}
+        
+        self.director_header = {
+            "Authorization": "Bearer {}".format(DIRECTOR_TOKEN)}
+
+        self.producer_header = {
+            "Authorization": "Bearer {}".format(PRODUCER_TOKEN)}
     
     def tearDown(self):
         """Executed after reach test"""
         pass
 
-    def home(self):
+    def test_home(self):
         res = self.client().get('/')
         data = json.loads(res.data)
+
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data["success"], True)
         self.assertTrue(data["condition"])
 
-    # Tests for casting assistant
-    def get_casting_assistant(self):
+   # Tests for casting assistant
+    def test_casting_assistant_actor(self):
         res = self.client().get('/actors', headers=self.assistant_header)
         data = json.loads(res.data)
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data["success"], True)
-        self.assertTrue(data['actors'])
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data["success"], False)
+        self.assertTrue(data['message'], 'unathorized' )
+
+    def test_actors(self):
+        res = self.client().get('/actors')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 401)
+
+    def test_casting_assistant_movies(self):
+        res = self.client().get('/movies', headers=self.assistant_header)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data["success"], False)
+        self.assertTrue(data['message'], 'unathorized' )
+
+    def test_movies(self):
+        res = self.client().get('/movies')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 401)
+        
+
+
+
+
+
+
+
 
 
 # Make the tests conveniently executable
