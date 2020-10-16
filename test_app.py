598b2fb409d9705f217020de83a1e83c53d61c92
smithhx <christian.smith.stanley@gmail.com>
2020-10-16 18:59:10 -0400

    commit test_app pycodestyle

--- test_app.py
+++ test_app.py
@@ -2,12 +2,12 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
-
 from app import create_app
 from models import setup_db, Movie, Actor
-#from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-#load_dotenv()
+# load_dotenv()
+
 
 class CapstoneTestCase(unittest.TestCase):
     """This class represents the capstone test case"""
@@ -17,30 +17,27 @@ class CapstoneTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "capstone"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        #self.database_path = os.environ.get['DATABASE_URL']
+        self.database_path = "postgres://{}/{}".format('localhost:5432',
+                                                       self.database_name)
+        # self.database_path = os.environ.get['DATABASE_URL']
         setup_db(self.app, self.database_path)
 
-        ASSISTANT_TOKEN = os.environ.get('ASSISTANT_TOKEN') 
+        ASSISTANT_TOKEN = os.environ.get('ASSISTANT_TOKEN')
         DIRECTOR_TOKEN = os.environ.get('DIRECTOR_TOKEN')
         PRODUCER_TOKEN = os.environ.get('PRODUCER_TOKEN')
 
-
         self.assistant_header = {
             "Authorization": "Bearer {}".format(ASSISTANT_TOKEN)}
-        
+
         self.director_header = {
             "Authorization": "Bearer {}".format(DIRECTOR_TOKEN)}
 
         self.producer_header = {
             "Authorization": "Bearer {}".format(PRODUCER_TOKEN)}
-            
 
-        #print('assistant', self.assistant_header)
-        #print('director', self.director_header)
-        #print('producer', self.producer_header)
-
-
+        # print('assistant', self.assistant_header)
+        # print('director', self.director_header)
+        # print('producer', self.producer_header)
 
         self.actor = {
             "name": "Will Smith",
@@ -81,14 +78,11 @@ class CapstoneTestCase(unittest.TestCase):
             'id': 7
         }
 
-
-
         with self.app.app_context():
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-
 
     def tearDown(self):
         """Executed after reach test"""
@@ -101,13 +95,13 @@ class CapstoneTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data["success"], True)
         self.assertTrue(data["condition"])
-    
+
     def test_401_casting_assistant_actor(self):
         res = self.client().get('/actors')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 401)
-    
+
     def test_401_casting_assistant_movies(self):
         res = self.client().get('/movies')
         data = json.loads(res.data)
@@ -145,10 +139,7 @@ class CapstoneTestCase(unittest.TestCase):
         self.assertEqual(data["success"], False)
         self.assertEqual(data['message'], 'unathorized')'''
 
-    
     # Tests for castins_director (POST/DELETE/PATCH)
-
-
     def test_casting_director_get_actor(self):
         res = self.client().get('/actors', headers=self.director_header)
         data = json.loads(res.data)
@@ -165,7 +156,8 @@ class CapstoneTestCase(unittest.TestCase):
         self.assertEqual(data['message'], 'unathorized')'''
 
     def test_post_casting_director_success(self):
-        res = self.client().post('/actors', json=self.add_actor, headers=self.director_header)
+        res = self.client().post('/actors', json=self.add_actor,
+                                 headers=self.director_header)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -185,13 +177,12 @@ class CapstoneTestCase(unittest.TestCase):
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 500)
         self.assertEqual(data['success'], False)
-    
+
     def test_post_casting_director(self):
         res = self.client().post('/actors', json=self.add_actor)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 401)
-
 
     def test_director_delete_actors(self):
         res = self.client().delete('actors/1', headers=self.director_header)
@@ -230,9 +221,7 @@ class CapstoneTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
-
     # Tests for PRODUCER (POST/DELETE/PATCH)
-
     def test_producer_get_actor(self):
         res = self.client().get('/actors', headers=self.producer_header)
         data = json.loads(res.data)
@@ -249,7 +238,8 @@ class CapstoneTestCase(unittest.TestCase):
         self.assertEqual(data['message'], 'unathorized')'''
 
     def test_post_producer_success(self):
-        res = self.client().post('/actors', json=self.add_actor, headers=self.producer_header)
+        res = self.client().post('/actors', json=self.add_actor,
+                                 headers=self.producer_header)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -269,13 +259,12 @@ class CapstoneTestCase(unittest.TestCase):
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 500)
         self.assertEqual(data['success'], False)
-    
+
     def test_post_producer(self):
         res = self.client().post('/actors', json=self.add_actor)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 401)
-
 
     def test_producer_delete_actors(self):
         res = self.client().delete('actors/1', headers=self.producer_header)
@@ -322,5 +311,7 @@ class CapstoneTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
 
 # Make the tests conveniently executable
+
+
 if __name__ == "__main__":
     unittest.main()
